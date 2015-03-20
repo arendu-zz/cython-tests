d297a4bd5f81899bb3895dc116b1eb9db22a7dbc
arendu <adithya.r@gmail.com>
2015-03-19 23:32:43 -0400

    set up for para2

--- setup3.py
+++ setup3.py
@@ -1,1 +1,16 @@
-__author__ = 'arenduchintala'
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_module = Extension(
+    "para2",
+    ["para2.pyx"],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-fopenmp'],
+)
+
+setup(
+    name='para2',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[ext_module],
+)
